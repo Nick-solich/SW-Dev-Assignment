f162a7504070cf38253796a1b0bf2244ff370cfa
Nantawat Rattanaton <nick.rattanaton@gmail.com>
2023-02-08 22:58:42 +0700

    Chapter07.02 MongoDB Connection Code Demo (2565)

--- VacQ/server.js
+++ VacQ/server.js
@@ -18,6 +18,7 @@ app.use(express.json());
 app.use("/api/v1/hospitals", hospitals);
 
 const PORT = process.env.PORT || 5000;
+
 const server = app.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
