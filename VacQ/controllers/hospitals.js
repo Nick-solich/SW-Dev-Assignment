e0a124c3cadc0397f1f93113fba2eaa925392e01
Nick <nick.rattanaton@gmail.com>
2023-03-16 20:35:49 +0700

    replace select sort

--- VacQ/controllers/hospitals.js
+++ VacQ/controllers/hospitals.js
@@ -4,8 +4,40 @@ const Hospital = require("../models/Hospital");
 //@route    GET /api/v1/hospitals
 //@access   Public
 exports.getHospitals = async (req, res, next) => {
+  let query;
+  //Copy req.query
+  const reqQuery = { ...req.query };
+
+  //Fields to exclude
+  const removeFields = ["select", "sort"];
+
+  //Loop over removeFields and delete them from reqQuery
+  removeFields.forEach((param) => delete reqQuery[param]);
+  console.log(reqQuery);
+
+  //Create query string
+  let queryStr = JSON.stringify(reqQuery);
+  queryStr = queryStr.replace(
+    /\b(gt|gte|lt|lte|in)\b/g,
+    (match) => `$${match}`
+  );
+  query = Hospital.find(JSON.parse(queryStr));
+
+  //Select fields
+  if (req.query.select) {
+    const fields = req.query.select.split(",").join(" ");
+    query = query.select(fields);
+  }
+
+  //Sort
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(",").join(" ");
+    query = query.sort(sortBy);
+  } else {
+    query = query.sort("-createdAt");
+  }
   try {
-    const hospitals = await Hospital.find();
+    const hospitals = await query;
     res
       .status(200)
       .json({ success: true, count: hospitals.length, data: hospitals });
