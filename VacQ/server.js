37e3fda98d7a83b0f4bc506e2432f537d020560e
Nick <nick.rattanaton@gmail.com>
2023-04-06 20:40:14 +0700

    🐱‍👤 Open API Hospital

--- VacQ/server.js
+++ VacQ/server.js
@@ -27,7 +27,7 @@ const app = express();
 
 const swaggerOptions = {
   swaggerDefinition: {
-    option: "3.0.0",
+    openapi: "3.0.0",
     info: {
       title: "Library API",
       version: "1.0.0",
